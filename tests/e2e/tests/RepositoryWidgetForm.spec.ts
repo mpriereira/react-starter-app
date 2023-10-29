56d285ca7924c58def64eb6f3fb691e8ebb37f6b
Mario <mprietopereira@gmail.com>
2023-10-29 10:00:23 +0100

    Fix e2e tests

--- tests/e2e/tests/RepositoryWidgetForm.spec.ts
+++ tests/e2e/tests/RepositoryWidgetForm.spec.ts
@@ -1,9 +1,5 @@
 import { RepositoryWidgetMother } from "../../RepositoryWidgetMother";
 
-before(() => {
-	localStorage.setItem("github_access_token", Cypress.env("api_token") as string);
-});
-
 describe("Repository Widget Form", () => {
 	it("Add new repository with id and url", () => {
 		cy.visit("/");
